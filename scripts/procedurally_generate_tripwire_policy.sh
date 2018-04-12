399c505389c7a337f05b7ba00a4e6805b5dd7dc9
SteffeyDev <steffeydev@icloud.com>
2018-04-12 14:43:37 -0400

    Minor change

--- scripts/procedurally_generate_tripwire_policy.sh
+++ scripts/procedurally_generate_tripwire_policy.sh
@@ -1,5 +1,5 @@
 for S in `ls /servers`; do
-  echo "( rulename = "Everything", severity = 100)">>~/twpol.txt
+  echo "( rulename = "$S", severity = 100)">>~/twpol.txt
   echo "{">>~/twpol.txt
   for D in `ls /servers/$S | grep -v dev | grep -v srv | grep -v proc`; do
     echo "/servers/$S/$D -> \$(ReadOnly) (recurse = true);">>~/twpol.txt
