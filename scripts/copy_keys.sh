253f1505f3a1a26eb377268d90b175774dc38fb3
Charlton Trezevant <ct@ctis.me>
2018-04-12 00:05:07 -0400

    Remove SSHFS setup from key copying script

--- scripts/copy_keys.sh
+++ scripts/copy_keys.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-sudo mkdir /servers
-sudo chown root:toads /servers
-sudo chmod 0775 /servers
-echo "#!/usr/bin/env bash" >>~/mount_sshfs.sh
-chmod +x ~/mount_sshfs.sh
-
 while [ true ]
 do
     echo "Enter the IP address of the machine:"
@@ -15,7 +9,6 @@ do
     ssh-copy-id root@$IP
     echo "$IP $HOSTNAME" | sudo tee -a /etc/hosts
     echo $HOSTNAME | sudo tee -a /etc/servers
-    echo "sshfs -o reconnect,ServerAliveInterval=20,allow_other root@$IP:/ /servers/$HOSTNAME" >>~/mount_sshfs.sh
     mkdir /servers/$HOSTNAME
     echo -e "All done!\n\n"
 done
