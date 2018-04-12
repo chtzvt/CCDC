3087c821cb6c50e8ecee2298e3d12171cf4ccecf
Charlton Trezevant <ct@ctis.me>
2018-04-11 23:52:22 -0400

    Update copy_keys to enable SSH mounts, etc

--- scripts/copy_keys.sh
+++ scripts/copy_keys.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+sudo mkdir /servers
+sudo chown root:toads /servers
+sudo chmod 0775 /servers
+echo "#!/usr/bin/env bash" >>~/mount_sshfs.sh
+chmod +x ~/mount_sshfs.sh
+
 while [ true ]
 do
     echo "Enter the IP address of the machine:"
@@ -7,6 +13,9 @@ do
     echo "What hostname should we resolve to $IP?"
     read HOSTNAME
     ssh-copy-id root@$IP
-    echo "$IP $HOSTNAME" >>/etc/hosts
+    echo "$IP $HOSTNAME" | sudo tee -a /etc/hosts
+    echo $HOSTNAME | sudo tee -a /etc/servers
+    echo "sshfs -o reconnect,ServerAliveInterval=20 root@$IP:/ /servers/$HOSTNAME" >>~/mount_sshfs.sh
+    mkdir /servers/$HOSTNAME
     echo -e "All done!\n\n"
 done
