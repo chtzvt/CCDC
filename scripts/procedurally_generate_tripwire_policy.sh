cf7c2cad7dddf0425b90d9f7bf8d390fff71a86b
SteffeyDev <steffeydev@icloud.com>
2018-04-12 00:57:54 -0400

    updating tripwire policy script

--- scripts/procedurally_generate_tripwire_policy.sh
+++ scripts/procedurally_generate_tripwire_policy.sh
@@ -1,5 +1,8 @@
-for S in `cat /etc/servers`; do
-  for D in `ls /servers/$S`; do
+for S in `ls /servers`; do
+  echo "( rulename = "Everything", severity = 100)">>~/twpol.txt
+  echo "{">>~/twpol.txt
+  for D in `ls /servers/$S | grep -v dev | grep -v srv | grep -v proc`; do
     echo "/servers/$S/$D -> \$(ReadOnly) (recurse = true);">>~/twpol.txt
   done
+  echo "}">>~/twpol.txt
 done
